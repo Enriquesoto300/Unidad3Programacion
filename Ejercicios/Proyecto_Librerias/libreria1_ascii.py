cce32bb100fbfeecc87f44614227a62fed23800f
Enriqueso300 <24040001@utculiacan.edu.mx>
2025-08-15 18:41:58 -0700

    Se añadieron diversos proyectos

---
+++ Ejercicios/Proyecto_Librerias/libreria1_ascii.py
@@ -0,0 +1,6 @@
+# libreria1_ascii.py
+
+def corazones(n):
+    """Imprime corazones en ASCII n veces"""
+    for i in range(n):
+        print(" <3 " * n)
---
+++ Ejercicios/Proyecto_Librerias/libreria3_mensajes.py
@@ -0,0 +1,4 @@
+# libreria3_mensajes.py
+
+mensaje_bienvenida = "¡Bienvenido al programa de arte ASCII y cálculos simples!"
+mensaje_despedida = "¡Gracias por usar el programa! Hasta luego."
